2b33e4fd02646638d6c88a1aac6ec76e48b28f92
caamaha <caamaha@qq.com>
2017-03-15 11:32:42 +0800

    ignore wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'bdm267573508_db');
+define('DB_NAME', 'wordpress');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'bdm267573508');
+define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'waitforfairy');
+define('DB_PASSWORD', 'root');
 
 /** MySQL主机 */
-define('DB_HOST', 'bdm267573508.my3w.com');
+define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
